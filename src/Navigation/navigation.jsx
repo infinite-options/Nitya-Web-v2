c4144a4564067656b0c7f3f6fbe33390404db6e6
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-01-11 07:16:43 -0800

    fixed navbar logo sizing

--- src/Navigation/navigation.jsx
+++ src/Navigation/navigation.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect, useRef } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
@@ -10,14 +8,11 @@ import Logo from "../Assets/Images/Logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
-
   const [open, setOpen] = useState(false);
-  const [projects, setProjects] = useState(false)
-  const [onClickAbout, setOnClickAbout] = useState(false)
-  const [onClickproject, setOnClickProject] = useState(false)
-  const [onClickteams, setOnClickTeams] = useState(false)
-
-
+  const [projects, setProjects] = useState(false);
+  const [onClickAbout, setOnClickAbout] = useState(false);
+  const [onClickproject, setOnClickProject] = useState(false);
+  const [onClickteams, setOnClickTeams] = useState(false);
 
   const handleClick = () => {
     setOpen(!open);
@@ -25,81 +20,102 @@ const Navbar = () => {
 
   const closeMenu = () => {
     setOpen(false);
-    setProjects(false)
-
+    setProjects(false);
   };
 
   const closeMenuHome = () => {
     setOpen(false);
-    setProjects(false)
-    setOnClickAbout(!onClickAbout)
-    console.log("about", onClickAbout)
-
+    setProjects(false);
+    setOnClickAbout(!onClickAbout);
+    console.log("about", onClickAbout);
   };
 
   const closeMenuProjects = () => {
     setOpen(false);
-    setProjects(true)
-    setOnClickProject(!onClickproject)
-    console.log("clickPro", onClickproject)
-
+    setProjects(true);
+    setOnClickProject(!onClickproject);
+    console.log("clickPro", onClickproject);
   };
 
   const closeMenuTeams = () => {
     setOpen(false);
-    setProjects(true)
-    setOnClickTeams(!onClickteams)
-    console.log("click", onClickteams)
+    setProjects(true);
+    setOnClickTeams(!onClickteams);
+    console.log("click", onClickteams);
   };
 
-
   return (
-  <nav className="navbar">
-    <div className="emptyDiv">
-      </div>
-      <Link to="/" className="nav-logo"  onClick={closeMenu}>
-         <img src={Logo} style={{height:'auto', width:'10rem'}}/>
+    <nav className="navbar">
+      <div className="emptyDiv"></div>
+      <Link to="/" className="nav-logobar" onClick={closeMenu}>
+        <img
+          className="nav-logo"
+          src={Logo}
+          // style={{
+          //   maxHeight: "100%",
+          //   height: "auto",
+          //   width: "10rem",
+          //   maxHeight: "100%",
+          //   objectFit: "contain",
+          // }}
+        />
       </Link>
       <div onClick={handleClick} className="nav-icon">
         {open ? <FiX /> : <FiMenu />}
       </div>
       <ul className={open ? "nav-links active" : "nav-links"}>
-        <li className="nav-item" >
-          <Link to={{pathname: '/', state_project : { project: onClickproject } }} className="nav-link" onClick={closeMenuProjects} >
-           Home
+        <li className="nav-item">
+          <Link
+            to={{ pathname: "/", state_project: { project: onClickproject } }}
+            className="nav-link"
+            onClick={closeMenuProjects}
+          >
+            Home
           </Link>
         </li>
         <li className="nav-item">
-          <Link to={{pathname: '/services', state_teams : { teams: onClickteams } }} className="nav-link" onClick={closeMenuTeams} >
+          <Link
+            to={{ pathname: "/services", state_teams: { teams: onClickteams } }}
+            className="nav-link"
+            onClick={closeMenuTeams}
+          >
             Services
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link to={{pathname: '/about', state : { click: onClickAbout } }}  className="nav-link" onClick={closeMenuHome} >
+          <Link
+            to={{ pathname: "/about", state: { click: onClickAbout } }}
+            className="nav-link"
+            onClick={closeMenuHome}
+          >
             About Us
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link to="/contact" className="nav-link" onClick={closeMenu} > 
+          <Link to="/contact" className="nav-link" onClick={closeMenu}>
             Contact
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link to={{pathname: '/blog', state : { click: onClickAbout } }}  className="nav-link" onClick={closeMenuHome} >
-           Blog
+          <Link
+            to={{ pathname: "/blog", state: { click: onClickAbout } }}
+            className="nav-link"
+            onClick={closeMenuHome}
+          >
+            Blog
           </Link>
         </li>
       </ul>
 
-      <div className="emptyDiv1" >
-      {/* <AccountCircleIcon 
+      <div className="emptyDiv1">
+        {/* <AccountCircleIcon 
       size='large'
       style={{color:'#ad8131', marginRight:'0.5rem', marginTop:'0.3rem'}}/> */}
-   
-      <InstagramIcon
+
+        <InstagramIcon
           fontSize="large"
           className="instagram-icon"
           onClick={(event) =>
