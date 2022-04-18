022dca8999d9c9b79a87d31563bb763ffc4c8434
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-04-18 09:27:20 -0700

    old image styling

--- src/Services/Treaments.jsx
+++ src/Services/Treaments.jsx
@@ -37,18 +37,21 @@ export default function Treatments() {
               <div
                 style={{
                   width: "100%",
-                  display: "flex",
+                  // display: "flex",
                   flex: "1",
-                  backgroundColor: "white",
-                  justifyContent: "center",
+                  // backgroundColor: "white",
+                  // justifyContent: "center",
                 }}
               >
                 <img
                   style={{
-                    maxWidth: "100%",
-                    maxHeight: "100%",
-                    objectFit: " scale-down",
-                    position: "bottom",
+                    // maxWidth: "100%",
+                    // maxHeight: "100%",
+                    // objectFit: " scale-down",
+                    // position: "bottom",
+                    width: "100%",
+                    minHeight: "100%",
+                    objectFit: "cover",
                   }}
                   variant="top"
                   src={filteredService.image_url}
